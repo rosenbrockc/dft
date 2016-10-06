a44f6938059932fc8954a117ac3ff6bb64e23979
Conrad Rosenbrock <rosenbrockc@gmail.com>
2016-10-06 13:19:14 -0600

    Revision 0.0.0

---
+++ pydft/bases/fourier.py
@@ -0,0 +1,3 @@
+"""Plane wave basis for the DFT solvers. Exposes the generic linear
+algebra functions required by the other solvers and methods.
+"""
---
+++ pydft/solvers/poisson.py
@@ -0,0 +1,4 @@
+"""Poisson solver to calculate the electron density self-interaction
+contribution to the potential in the DFT formalism.
+"""
+
